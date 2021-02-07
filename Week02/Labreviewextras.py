bf92ebec79879baf8a1e0776f30b6a47df858364
Curry <G00074924@gmit.ie>
2021-02-07 15:34:11 +0000

    week02

---
+++ Week02/Labreviewextras.py
@@ -0,0 +1,19 @@
+# Extra Questions on lab 2.2 week 2
+# Author Veronica Curry
+# reviewing lectures and labs to practice
+
+#17
+name = input (' Enter your name; ')
+age = input (' Enter your age; ')
+print('Hello {}, your age is {}.'.format(name, age))
+
+#18
+name = input (' Enter your name; ')
+age = input (' Enter your age; ')
+print('Hello {},\tyour age is {}.'.format(name, age))
+
+
+#19
+answer = 21 - 4
+print(answer)
+
