152ea0cebf46e55774e48227760a2331a054e02c
Curry <G00074924@gmit.ie>
2021-02-07 15:15:05 +0000

    week02

---
+++ Week02/Labreview.py
@@ -0,0 +1,24 @@
+# review of week 2 material - refamiliariation of subject
+# Veronica Curry
+# revision
+
+# lab part 1 - Hello World
+
+print('Hello World Again')
+
+# remember to check for caps when writing code Answer is not the same as answer
+
+answer = 111 * 555
+print(answer)
+
+# 12  ## note print cannot be a capital 
+name = input ( 'Enter your name')
+print('Hello' + name)
+
+# add in a new line with response to question
+name = input ('Enter your name')
+print('Hello' + name + '\nNice to meet you')
+
+# add in new line with response to question with format
+name = input ('Enter your name')
+print('Hello {}\nNice to meet you'.format (name))
