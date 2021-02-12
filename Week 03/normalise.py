3debda03dec12bbae9d2b7b4bdefc3ada164e324
Curry <G00074924@gmit.ie>
2021-02-12 16:53:50 +0000

    week 03

---
+++ Week 03/normalise.py
@@ -0,0 +1,6 @@
+# This program reads in a string and strips
+# any leading or trailing spaces
+# It also converts all the letter to lower case
+# this program also outputs the length of the original string
+# And the normalised one
+
--- Week 03/randomgenerator.py
+++ Week 03/randomgenerator.py
@@ -4,3 +4,13 @@ import random
 
 number = random.randint (1,10)
 print(" here is a random number {}".format(number))
+
+
+# program which choose an answer from a random list of options
+
+import random
+
+mylist = [ "reading", "sleeping", "watching Dragrace!", "gin  and tonic", "walking", "Husband"]
+answer =(random.choice(mylist))
+
+print(" What should I do tonight?\t Answer is {}".format(answer))
