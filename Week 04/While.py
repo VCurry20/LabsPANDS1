4243803f449b33afb39510dd90a1c39126b46f44
Curry <G00074924@gmit.ie>
2021-02-15 22:14:03 +0000

    week 04

---
+++ Week 04/While.py
@@ -0,0 +1,17 @@
+# Lecture 4.2 While 
+# Monkey See Monkey Do from Lecture
+
+# Whlie Conditions:
+#       Statements
+
+count = 0
+while count < 10 :
+    print (count)
+    count += 1
+
+
+count = 10
+while count >= 0:
+    print (count)
+    count -= 1
+print ("Blast OFF")
