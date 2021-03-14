579dfc4ddedf1cc29d69d4b99f981350628e5e6f
Curry <G00074924@gmit.ie>
2021-03-14 19:36:08 +0000

    test

---
+++ Week 05/WeekdayTEST.py
@@ -0,0 +1,23 @@
+# Write a program that outputs whether or not today is a weekday
+
+# Week 5
+# Task 4
+
+# Veronica Curry
+# Student ID: G00074924
+
+
+# Answer - 
+# Please enter a day of the week
+# Answer - "Yes, unfortunately its a weekday" or "It is the weekend, yay!" 
+
+
+# Methodology
+## For loops and list of days of the week
+## Array / list of days?
+
+# daysofWeek = (Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)
+# weekend = last two of this list - 5, 6
+
+# if when x do a and if not do b
+
