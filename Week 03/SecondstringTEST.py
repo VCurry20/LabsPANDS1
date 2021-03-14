95bc59f5ab899954331902be206545b7cc0db663
Curry <G00074924@gmit.ie>
2021-03-14 17:46:34 +0000

    updating

---
+++ Week 03/SecondstringTEST.py
@@ -0,0 +1,21 @@
+# Write a program asks for input - string; outputs every second letter in reverse
+# Week 3
+# Task 2
+
+# Veronica Curry
+# Student ID: G00074924
+
+# sentence to enter: The quick brown fox jumps over the lazy dog.
+
+# Methodology:
+# need to figure out how to reverse a phrase [::-1]
+# delete every second letter or choose only every second letter [::-2]
+# how to choose everysecond letter  // Slicing 
+# include the full stop
+
+
+## References:
+# https://www.youtube.com/watch?v=ajrtAuDg3yw
+# https://www.w3schools.com/python/python_howto_reverse_string.asp
+
+
