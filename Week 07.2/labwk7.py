68a3f00a0a60a9364055212f900ee7c88f0c5756
Curry <G00074924@gmit.ie>
2021-04-07 19:10:07 +0100

    Week 7

---
+++ Week 07.2/labwk7.py
@@ -0,0 +1,10 @@
+# Week 7 Lab 
+
+# Quiz - 1
+# A - Nothing will print
+# B - another line - its set as "w" which will overwrite   7 first then 13 
+# C - another line - its set as "w" which will overwrite
+# D - another line ??
+
+
+
