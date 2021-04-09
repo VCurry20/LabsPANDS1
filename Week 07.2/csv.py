0f36bd510fb29443dbc639a8fc48e423cd3e4850
Curry <G00074924@gmit.ie>
2021-04-09 17:23:41 +0100

    update

---
+++ Week 07.2/csv.py
@@ -0,0 +1,14 @@
+# video extra
+
+# testing CSV
+
+import csv
+
+filename = "test.csv"
+
+#with open (filename, "rt") as csvFile:
+ #   csvdone = csv.reader (csvFile, delimiter= ",")
+  #  for line in csvdone:
+   #     print(line[2])
+
+
